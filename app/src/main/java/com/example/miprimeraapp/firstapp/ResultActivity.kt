3ff1a9f8f71d97ed5f1fd2fbb178bff66040ddef
Hugofullstack <hugo.n.montefusco@gmail.com>
2024-09-19 12:20:17 -0300

    Initial commit

---
+++ app/src/main/java/com/example/miprimeraapp/firstapp/ResultActivity.kt
@@ -0,0 +1,18 @@
+package com.example.miprimeraapp.firstapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.miprimeraapp.R
+
+class ResultActivity : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola amigo $name"
+        }
+    }
